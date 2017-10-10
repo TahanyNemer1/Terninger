6899ca760579f008dc044ac0939805d241d513ab
TahanyNemer1 <32563912+TahanyNemer1@users.noreply.github.com>
2017-10-10 16:52:48 +0200

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -10,11 +10,7 @@ public class Dice {
 	}
 	
 	// roll the die and return the value (1-6)
-	public int roll() {
-//		float d1 = (float)Math.random();     // 0-1
-//		float d2 = d1 * 5;                     // 0-5
-//		int d3 = Math.round(d2);             // 0-5 integer
-//		return d3 + 1;                     // 1-6		
+	public int roll() {	
 		int d = rand.nextInt(6)+1;
 		return d;
 	}
